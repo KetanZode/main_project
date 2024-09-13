1536441634c71f9adfdd722faae15ad9b9a733da
KetanZode <ketanzode2104@gmail.com>
2024-09-14 00:54:53 +0530

    Created views adding Uni

--- mainproject/admit/views.py
+++ mainproject/admit/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.views import View
+from .models import *
 # Create your views here.
 
 class MainView(View):
     def get(self, request):
-        return render(request, 'main.html')
+        university = University.objects.all()
+        context = {
+            'university' : university
+        }
+        return render(request, 'main.html', context)
+
+    def post(self, request):
+        print(request.POST)
+        data = request.POST
+        button_type = request.POST.get('buttonType', None)
+
+        if button_type == 'create_uni':
+            name = request.POST.get('uni_name')
+            daad_url = request.POST.get('daad_url')   
+            University.objects.create(name=name,daad_url=daad_url)  
+
+        return self.get(request)
 
